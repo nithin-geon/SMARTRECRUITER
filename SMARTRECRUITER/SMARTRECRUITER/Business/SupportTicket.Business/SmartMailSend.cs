e0e41df09491f0573eca4925e33a2abd40674a76
Nithin Das H <nithin.h@gadgeon.com>
2018-12-07 15:49:55 +0530

    Uploading the Project SmartRecruiters

---
+++ SMARTRECRUITER/SMARTRECRUITER/Business/SupportTicket.Business/SmartMailSend.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Net.Mail;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRecruiter.Business
+{
+    public class SmartMailSend
+    {
+
+       
+
+
+    }
+}
